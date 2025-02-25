81437e1d0168fd375e944d79f885cd595ec84ec1
syatirazulaika <syatirazulaika@gmail.com>
2025-02-25 16:27:04 +0700

    Studio.java

---
+++ Studio.java
@@ -0,0 +1,11 @@
+public class Studio {
+    int nomorStudio;
+    int kapasitasKursi;
+    Film filmDiputar;
+
+    public Studio(int nomorStudio, int kapasitasKursi, Film filmDiputar) {
+        this.nomorStudio = nomorStudio;
+        this.kapasitasKursi = kapasitasKursi;
+        this.filmDiputar = filmDiputar;
+    }
+}
