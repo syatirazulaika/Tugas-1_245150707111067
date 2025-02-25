e73e80c0ef4ccae27fc6eaef2ced6083b7dd70d2
syatirazulaika <syatirazulaika@gmail.com>
2025-02-25 19:49:42 +0700

    Film.java

---
+++ Film.java
@@ -0,0 +1,15 @@
+package Praktikum1;
+
+public class Film {
+    String judul;
+    String genre;
+    String jamTayang;
+    int hargaTiket;
+
+    public Film(String judul, String genre, String jamTayang, int hargaTiket) {
+        this.judul = judul;
+        this.genre = genre;
+        this.jamTayang = jamTayang;
+        this.hargaTiket = hargaTiket;
+    }
+}
