f51eda276c059baffa7a0d4712c5e379391f8071
syatirazulaika <syatirazulaika@gmail.com>
2025-02-25 19:51:53 +0700

    Tiket.java

---
+++ Tiket.java
@@ -0,0 +1,30 @@
+package Praktikum1;
+
+public class Tiket {
+    int nomorTiket;
+    Film film;
+    Studio studio;
+    int nomorKursi;
+    int hargaTiket;
+    Penonton pembeli;
+
+    public Tiket(int nomorTiket, Film film, Studio studio, int nomorKursi, Penonton pembeli) {
+        this.nomorTiket = nomorTiket;
+        this.film = film;
+        this.studio = studio;
+        this.nomorKursi = nomorKursi;
+        this.pembeli = pembeli;
+        this.hargaTiket = film.hargaTiket;
+    }
+
+    public void displayInfo() {
+        System.out.println("No. Tiket    : #" + nomorTiket);
+        System.out.println("Nama Penonton: " + pembeli.namaPenonton);
+        System.out.println("Film         : " + film.judul);
+        System.out.println("Genre        : " + film.genre);
+        System.out.println("Jam Tayang   : " + film.jamTayang);
+        System.out.println("Studio       : " + studio.nomorStudio);
+        System.out.println("No. Kursi    : " + nomorKursi);
+        System.out.println("Harga Tiket  : Rp " + hargaTiket);
+    }
+}
